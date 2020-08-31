a5e81efd74b29c300a216bba93f0dde432ee8422
RemyLabecot <remylabecot@gmail.com>
2020-08-31 21:22:26 +0200

    first commit

---
+++ src/javascript.js
@@ -0,0 +1,59 @@
+const getRandomSize = (min, max) => {
+
+    return Math.random() * (max - min) + min;
+}
+
+const randomizeColor = () => {
+
+    const hexaChar = '0123456789ABCDEF';
+    let hexaColor = '#';
+
+    for (let i=0;i<6;i++) {
+        hexaColor += hexaChar[Math.floor(Math.random() * 16)];
+    }
+    return hexaColor;
+}
+
+
+const windowWidth = window.innerWidth;
+const windowHeight = window.innerHeight;
+let squareSize = 200;
+
+window.onload = () => {
+    
+    const baseSquare = document.querySelector('.square');
+    const randomBaseSquareTop = getRandomSize(0, windowHeight);
+    const randomBaseSquareLeft = getRandomSize(0, windowWidth);
+
+    baseSquare.style.top = randomBaseSquareTop + 'px';
+    baseSquare.style.left = randomBaseSquareLeft + 'px';
+    baseSquare.style.height = squareSize + 'px';
+    baseSquare.style.width = squareSize + 'px';
+    baseSquare.style.backgroundColor = randomizeColor();
+}
+
+const splitSquareOnClick = () => {
+
+    squareSize = squareSize / 2;
+
+    for(let i = 0;i<4;i++) {
+        const newSquare = generateNewSquare();
+        document.body.appendChild(newSquare);
+    }
+}
+
+const generateNewSquare = () => {
+    const randomTop = getRandomSize(0, windowHeight);
+    const randomLeft = getRandomSize(0, windowWidth);
+    const newSquare = document.createElement('div');
+    newSquare.setAttribute('class', 'square');
+    newSquare.setAttribute("onclick","splitSquareOnClick();");
+
+    const newSize = squareSize;
+    newSquare.style.width = newSize + 'px';
+    newSquare.style.height = newSize + 'px';
+    newSquare.style.top = randomTop + 'px';
+    newSquare.style.left = randomLeft + 'px';
+    newSquare.style.backgroundColor = randomizeColor();
+    return newSquare;
+}
