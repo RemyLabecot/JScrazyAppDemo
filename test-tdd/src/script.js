b841715c6f41b6ce7c69d12e3ca8398e1249ff52
RemyLabecot <remylabecot@gmail.com>
2020-09-03 00:34:18 +0200

    (feat) add generic split square with id

--- test-tdd/src/script.js
+++ test-tdd/src/script.js
@@ -5,14 +5,23 @@ window.onload = () => {
     const square = document.getElementById('1');
     square.style.top = position[0] + 'px';
     square.style.left = position[1] + 'px';
+    square.style.width = 400 + 'px';
+    square.style.height = 400 + 'px';
 };
 
 const generateRandomSquarePosition = (squareSize, randomFactor, windowHeight, windowWidth) => {
 
     const distanceOfTop = (windowHeight - squareSize) * randomFactor;
     const distanceOfLeft = (windowWidth - squareSize) * randomFactor;
-
     return [distanceOfTop, distanceOfLeft];
 };
 
-module.exports = generateRandomSquarePosition;
+const splitSquareSize = (id) => {
+
+    const squareSize = getSquareById(id).offsetHeight / 2;
+    return squareSize + 'px';
+};
+
+const getSquareById = (id) => {return document.getElementById(id);};
+
+module.exports = {generateRandomSquarePosition, splitSquareSize};
--- test-tdd/test/script.test.js
+++ test-tdd/test/script.test.js
@@ -1,5 +1,12 @@
-const generateRandomSquarePosition = require('../src/script');
+import * as script from '../src/script';
 
-test('generateRandomSquarePosition return valid position', () => {
-    expect(generateRandomSquarePosition(400, 1, 500, 500)).toStrictEqual([100, 100]);
+test('generateRandomSquarePosition must return valid position', () => {
+    expect(script.generateRandomSquarePosition(400, 1, 500, 500)).toStrictEqual([100, 100]);
 });
+
+/*test('splitSquareOnClick must split square size by 2', () => {
+
+    document.body.innerHTML = `<div id="1" class="square" onclick="splitSquareSize(this.id)" style="top: 42.467px; left: 260.931px; width: 400px; height: 400px;"></div>`;
+
+    expect(script.splitSquareSize(1)).toBe('200px');
+});*/
