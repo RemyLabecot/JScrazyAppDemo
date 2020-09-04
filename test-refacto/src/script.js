b4ff4f30e68eda46f3e13d4cac50dbe844234fe4
RemyLabecot <remylabecot@gmail.com>
2020-09-04 19:44:50 +0200

    (refacto) complet et ajout crazy mode

--- test-refacto/src/script.js
+++ test-refacto/src/script.js
@@ -1,63 +1,71 @@
+let crazyMode;
+
 window.onload = () => {
 
-    // Random position of the first square on load
-    const square = document.getElementById('1');
-    const position = generateRandomSquarePosition(400, Math.random(), window.innerHeight, window.innerWidth);
-
-    square.style.top = position[0] + 'px';
-    square.style.left = position[1] + 'px';
+    // Generate first square
+    document.body.appendChild(generateNewSquare(400, '0'));
 };
 
-const split = (id) => {
+window.ondblclick = () => {
 
-    const square = getSquareById(id);
-    const newSquareSize = splitSquareSize(square);
+    if (crazyMode > 0) {
+        clearTimeout(crazyMode);
+        crazyMode = 0;
+    } else {
+        activateCrazyMode();
+    }
+};
+
+const split = (event) => {
+
+    const square = document.getElementById(event.target.id);
+    const newSquareSize = square.offsetHeight / 2;
 
     for(let i=0;i<4;i++) {
-        const newSquare = generateNewSquare(newSquareSize, square.id);
-        document.body.appendChild(newSquare);
+        document.body.appendChild(generateNewSquare(newSquareSize, event.target.id));
     }
 };
 
 const generateNewSquare = (squareSize, id) => {
-    // A REFACTO
-    const position = generateRandomSquarePosition(squareSize, Math.random(), window.innerHeight, window.innerWidth);
-    const newSquare = document.createElement('div');
 
+    const newSquare = document.createElement('div');
+    const position = generateRandomSquarePosition(squareSize, Math.random(), window.innerHeight, window.innerWidth);
     newSquare.setAttribute('class', 'square');
     newSquare.style.top = position[0] + 'px';
     newSquare.style.left = position[1] + 'px';
-    newSquare.style.width = squareSize;
-    newSquare.style.height = squareSize;
-    newSquare.id = Number(id) + 1;
+    newSquare.style.width = squareSize + 'px';
+    newSquare.style.height = squareSize + 'px';
+    newSquare.style.backgroundColor = randomizeColor();
+    newSquare.onclick = split;
+    newSquare.id = parseInt(id, 10) + 1;
 
     return newSquare;
 };
-
-const generateRandomSquarePosition = (squareSize, randomFactor, windowHeight, windowWidth) => {
-
-    const distanceOfTop = (windowHeight - squareSize) * randomFactor;
-    const distanceOfLeft = (windowWidth - squareSize) * randomFactor;
-    return [distanceOfTop, distanceOfLeft];
-};
-
-const splitSquareSize = (square) => {
-
-    const squareSize = square.offsetHeight / 2;
-    return squareSize + 'px';
-};
-
-const getSquareById = (id) => {return document.getElementById(id);};
 
 const randomizeColor = () => {
 
     const hexaChar = '0123456789ABCDEF';
     let hexaColor = '#';
-
     for (let i=0;i<6;i++) {
         hexaColor += hexaChar[Math.floor(Math.random() * 16)];
     }
     return hexaColor;
 };
 
-module.exports = {generateRandomSquarePosition, splitSquareSize, getSquareById};
+const generateRandomSquarePosition = (squareSize, randomFactor, windowHeight, windowWidth) => {
+    const distanceOfTop = (windowHeight - squareSize) * randomFactor;
+    const distanceOfLeft = (windowWidth - squareSize) * randomFactor;
+    return [distanceOfTop, distanceOfLeft];
+};
+
+const activateCrazyMode = () => {
+
+    const elements = document.getElementsByClassName('square');
+    for (let element of elements) {
+        element.style.top = generateRandomSquarePosition(element.offsetHeight, Math.random(), window.innerHeight, window.innerWidth)[0] + 'px';
+        element.style.left = generateRandomSquarePosition(element.offsetWidth, Math.random(), window.innerHeight, window.innerWidth)[1] + 'px';
+    }
+    crazyMode = setTimeout(() => {
+        activateCrazyMode();
+    }, 2000);
+};
